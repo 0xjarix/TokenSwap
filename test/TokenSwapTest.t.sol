a02a298ac51d249147c29ef213d3c016092b79dd
joseph abou rizk <joseph.abou-rizk@epita.fr>
2024-01-16 10:20:18 +0100

    test fixed

--- test/TokenSwapTest.t.sol
+++ test/TokenSwapTest.t.sol
@@ -11,8 +11,8 @@ contract TokenSwapTest is Test {
     TokenSwap tokenSwap;
     TokenA tokenA;
     TokenB tokenB;
+    address alice;
     function setUp() public virtual {
-        vm.prank(address(this));
         tokenSwap = new TokenSwap();
         tokenA = tokenSwap.tokenA();
         tokenB = tokenSwap.tokenB();
@@ -21,30 +21,31 @@ contract TokenSwapTest is Test {
         console.log("tokenSwap address:", address(tokenSwap));
         console.log("tokenSwap balance tokenA:", tokenA.balanceOf(address(tokenSwap)));
         console.log("tokenSwap balance tokenB:", tokenB.balanceOf(address(tokenSwap)));
+        alice = address(0x3);
+        tokenA.mint(alice, 100000);
+        tokenB.mint(alice, 100000);
     }
 
     function testTokenASwappedForTokenB() public {
-        console.log("tokenB balance:", tokenB.balanceOf(address(tokenSwap)));
-        address alice = address(0x3);
-        tokenA.mint(alice, 100000);
-        console.log("tokenA balance:", tokenA.balanceOf(alice));
-        vm.prank(alice);
+        vm.startPrank(alice);
+        uint256 tokenA_balance = tokenA.balanceOf(alice);
+        uint256 tokenB_balance = tokenB.balanceOf(alice);
         tokenSwap.swapTokenAForTokenB(100);
-        assertEq(tokenA.balanceOf(alice), 0);
-        assertEq(tokenB.balanceOf(alice), 200);
+        assertEq(tokenA.balanceOf(alice), tokenA_balance - 100);
+        assertEq(tokenB.balanceOf(alice), tokenB_balance + 200);
     }
     
     function testTokenBSwappedForTokenA() public {
-        address alice = address(0x3);
-        tokenB.mint(alice, 100000);
-        vm.prank(alice);
+        vm.startPrank(alice);
+        uint256 tokenA_balance = tokenA.balanceOf(alice);
+        uint256 tokenB_balance = tokenB.balanceOf(alice);
         tokenSwap.swapTokenBForTokenA(100);
-        assertEq(tokenB.balanceOf(alice), 0);
-        assertEq(tokenA.balanceOf(alice), 50);
+        assertEq(tokenB.balanceOf(alice), tokenB_balance - 100);
+        assertEq(tokenA.balanceOf(alice), tokenA_balance + 50);
     }
 
     function testRecoverTokenAByOwner() public {
-        vm.prank(address(this));
+        vm.startPrank(address(this));
         uint256 tokenA_balance = tokenA.balanceOf(address(tokenSwap));
         tokenSwap.recoverTokenA();
         assertEq(tokenA.balanceOf(address(this)), tokenA_balance);
@@ -52,14 +53,13 @@ contract TokenSwapTest is Test {
     }
 
     function testRecoverTokenAByNonOwner() public {
-        address alice = address(0x3);
-        vm.prank(alice);
+        vm.startPrank(alice);
         vm.expectRevert();
         tokenSwap.recoverTokenA();
     }
 
     function testRecoverTokenBByOwner() public {
-        vm.prank(address(this));
+        vm.startPrank(address(this));
         uint256 tokenB_balance = tokenB.balanceOf(address(tokenSwap));
         tokenSwap.recoverTokenB();
         assertEq(tokenB.balanceOf(address(this)), tokenB_balance);
@@ -67,8 +67,7 @@ contract TokenSwapTest is Test {
     }
 
     function testRecoverTokenBByNonOwner() public {
-        address alice = address(0x3);
-        vm.prank(alice);
+        vm.startPrank(alice);
         vm.expectRevert();
         tokenSwap.recoverTokenB();
     }
